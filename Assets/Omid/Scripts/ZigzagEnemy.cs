75327e97c468c335768ea3e5c278ffb39880b6a8
grosspengu1n <Ismailyomid@gmail.com>
2024-03-05 16:06:15 +0100

    [add] enemy

---
+++ Assets/Omid/Scripts/ZigzagEnemy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ZigzagEnemy : MonoBehaviour
+{
+    public float speed = 5f;
+    public float zigzagSpeed = 2f; 
+    public float amplitude = 2f; 
+    public float frequency = 1f; 
+
+
+    private Transform player; 
+    private bool moveLeft = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        if (transform.position.y < player.position.y && !moveLeft)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+            if (transform.position.y >= player.position.y)
+            {
+                moveLeft = true;
+            }
+        }
+        else
+        {
+
+            
+            float yPosition = Mathf.Sin(Time.time * frequency) * amplitude;
+            transform.Translate(Vector3.up * yPosition * zigzagSpeed * Time.deltaTime);
+
+        }
+    }
+}
+
