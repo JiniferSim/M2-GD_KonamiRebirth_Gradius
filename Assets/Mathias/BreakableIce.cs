812fd8c0adfbe2068353c301fdd0f6eb379ab2a5
bludg0d <mathias.lust@artfx.fr>
2024-03-10 14:57:58 +0100

    [Add] Level Set

---
+++ Assets/Mathias/BreakableIce.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableIce : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    Color originalColor;
+    float flashDuration = 1f;
+
+
+    public int HP = 3;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        originalColor = meshRenderer.material.color;
+    }
+    private void Update()
+    {
+        if (HP <= 0)
+        {
+            SelfDestroy();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(other);
+
+            FlashStart();
+            HP--;
+        }
+    }
+
+    private void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void FlashStart()
+    {
+        meshRenderer.material.color = Color.white;
+        Invoke("FlashStop", flashDuration);
+    }
+    private void FlashStop()
+    {
+        meshRenderer.material.color = originalColor;
+
+    }
+}
---
+++ Assets/Mathias/VolcanoHitbox.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoHitbox : MonoBehaviour
+{
+    
+    public GameObject whiteMesh;
+    public float flashDuration = 1f;
+
+
+    public int HP = 20;
+
+    private void Start()
+    {
+      
+    }
+    private void Update()
+    {
+        if (HP <= 0)
+        {
+            SelfDestroy();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(other);
+
+            FlashStart();
+            HP--;
+        }
+    }
+
+    private void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void FlashStart()
+    {
+        whiteMesh.SetActive(true);
+        Invoke("FlashStop", flashDuration);
+    }
+    private void FlashStop()
+    {
+        whiteMesh.SetActive(false);
+    }
+}
