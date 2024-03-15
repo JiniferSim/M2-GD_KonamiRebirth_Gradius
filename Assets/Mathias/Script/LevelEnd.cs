5eb0b1ef9b189a8af899a6087f2a59fe4c87f556
grosspengu1n <Ismailyomid@gmail.com>
2024-03-15 22:48:16 +0100

    [add] rndm

--- Assets/Mathias/Script/LevelEnd.cs
+++ Assets/Mathias/Script/LevelEnd.cs
@@ -1,27 +1,41 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class LevelEnd : MonoBehaviour
 {
     public ParticleSystem sunDeath;
     public GameObject bottomBar, topScore, centerScore;
     public GameObject[] celeb;
-    public int current = 0;
     public Text centerScoreText;
     public float interval;
     bool switched;
+
+    private List<int> availableIndices = new List<int>();
+    private int current = 0;
+
+    private void Start()
+    {
+        InitializeAvailableIndices();
+    }
+
+    private void InitializeAvailableIndices()
+    {
+        for (int i = 0; i < celeb.Length; i++)
+        {
+            availableIndices.Add(i);
+        }
+        Shuffle(availableIndices);
+    }
+
     private void Update()
     {
         centerScoreText.text = "" + SpaceshipController.score;
     }
+
     public void OnTriggerEnter(Collider other)
     {
-       
         if (other.CompareTag("Player"))
         {
             bottomBar.SetActive(false);
@@ -30,17 +44,38 @@ public class LevelEnd : MonoBehaviour
             Invoke("NextScene", 15);
         }
     }
-    void Celebrate()
+
+    private void Celebrate()
     {
+        if (availableIndices.Count == 0)
+        {
+            Debug.Log("All indices used up!");
+            return;
+        }
+
         switched = !switched;
         centerScore.SetActive(switched);
-        ParticleSystem sunParticlo = Instantiate(sunDeath, celeb[current].transform.position, Quaternion.identity);
-        Destroy(sunParticlo, 5f);
-        current = (current + 1) % 6;
+
+        int randomIndex = availableIndices[0];
+        availableIndices.RemoveAt(0);
+
+        ParticleSystem sunParticle = Instantiate(sunDeath, celeb[randomIndex].transform.position, Quaternion.identity);
+        Destroy(sunParticle, 5f);
     }
 
-    void NextScene()
+    private void NextScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void Shuffle<T>(List<T> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
     }
 }
